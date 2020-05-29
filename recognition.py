dfd7c1a8ede2678275872c8cbc13de18cd9d5adc
Skadi <oswinwuo.o@gmail.com>
2020-05-29 11:04:43 +0800

    first update

---
+++ recognition.py
@@ -0,0 +1,56 @@
+import cv2
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read('./face_train_data/trainer.yml')
+cascadePath = "./cascades/haarcascade_frontalface_default.xml"
+faceCascade = cv2.CascadeClassifier(cascadePath)
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+idnum = 0
+
+names = ['Unkown', 'Cook']
+
+cam = cv2.VideoCapture(1)
+minW = 0.1*cam.get(3)
+minH = 0.1*cam.get(4)
+
+# 设定写入参数
+fps = 30
+size = (int(cam.get(cv2.CAP_PROP_FRAME_WIDTH)),
+        int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT)))
+fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+vout_1 = cv2.VideoWriter()
+vout_1.open('./output/output_rec.mp4', fourcc, fps, size, True)
+
+while True:
+    ret, img = cam.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.2,
+        minNeighbors=5,
+        minSize=(int(minW), int(minH))
+    )
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        idnum, confidence = recognizer.predict(gray[y:y+h, x:x+w])
+
+        if confidence < 100:
+            idnum = names[idnum]
+            confidence = "{0}%".format(round(100 - confidence))
+        else:
+            idnum = "unknown"
+            confidence = "{0}%".format(round(100 - confidence))
+
+        cv2.putText(img, str(idnum), (x+5, y-5), font, 1, (0, 0, 255), 1)
+        cv2.putText(img, str(confidence), (x+5, y+h-5), font, 1, (0, 0, 0), 1)
+    vout_1.write(img)
+    cv2.imshow('camera', img)
+    k = cv2.waitKey(10)
+    if k == 27:
+        break
+
+cam.release()
+cv2.destroyAllWindows()
